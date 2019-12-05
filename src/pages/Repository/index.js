f6171feb53aedf261fd2fa987f66868628c4f54a
Renan Oliveira <renanlido@yahoo.com.br>
2019-12-05 02:41:19 -0200

    Feature - Add filter and pagination

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-
 import api from '../../services/api';
 
 import Container from '../../components/Container';
-import { Loading, Owner, IssueList, User } from './styles';
+import {
+  Loading,
+  Owner,
+  IssueList,
+  User,
+  IssueFilter,
+  PageActions,
+} from './styles';
 
 class Repository extends Component {
   state = {
     repository: {},
     issues: [],
     loading: 1,
+    filters: [
+      { state: 'all', label: 'Todas', active: true },
+      { state: 'open', label: 'Abertas', active: false },
+      { state: 'closed', label: 'Fechadas', active: false },
+    ],
+    filterIndex: 0,
+    page: 1,
+    disabledNext: 1,
+    disabledBack: 1,
   };
 
   async componentDidMount() {
     const { match } = this.props;
+    const { filters } = this.state;
 
     const repoName = decodeURIComponent(match.params.repository);
 
@@ -23,7 +39,7 @@ class Repository extends Component {
       api.get(`/repos/${repoName}`),
       api.get(`/repos/${repoName}/issues`, {
         params: {
-          state: 'open',
+          state: filters.find(filter => filter.active).state,
           per_page: 5,
         },
       }),
@@ -34,10 +50,80 @@ class Repository extends Component {
       issues: issues.data,
       loading: 0,
     });
+
+    this.changePropsDisabled();
   }
 
+  loadIssues = async () => {
+    const { match } = this.props;
+    const { filters, filterIndex, page } = this.state;
+
+    const repoName = decodeURIComponent(match.params.repository);
+
+    const response = await api.get(`/repos/${repoName}/issues`, {
+      params: {
+        state: filters[filterIndex].state,
+        per_page: 5,
+        page,
+      },
+    });
+
+    this.setState({ issues: response.data });
+  };
+
+  handleFilterClick = async filterIndex => {
+    await this.setState({ filterIndex, page: 1 });
+    this.changePropsDisabled();
+    this.loadIssues();
+  };
+
+  handlePage = async action => {
+    const { page } = this.state;
+    await this.setState({
+      page: action === 'back' ? page - 1 : page + 1,
+    });
+    this.changePropsDisabled();
+    this.loadIssues();
+  };
+
+  changePropsDisabled = async () => {
+    const { match } = this.props;
+    const { filters, filterIndex, page } = this.state;
+
+    const repoName = decodeURIComponent(match.params.repository);
+
+    const testNextPage = await api.get(`/repos/${repoName}/issues`, {
+      params: {
+        state: filters[filterIndex].state,
+        per_page: 5,
+        page: page + 1,
+      },
+    });
+
+    if (page < 2) {
+      this.setState({ disabledNext: 1 });
+    } else {
+      this.setState({ disabledNext: 0 });
+    }
+
+    if (testNextPage.data.length < 1) {
+      this.setState({ disabledBack: 1 });
+    } else {
+      this.setState({ disabledBack: 0 });
+    }
+  };
+
   render() {
-    const { repository, issues, loading } = this.state;
+    const {
+      repository,
+      issues,
+      loading,
+      filters,
+      filterIndex,
+      page,
+      disabledNext,
+      disabledBack,
+    } = this.state;
 
     if (loading) {
       return <Loading>Carregando...</Loading>;
@@ -53,6 +139,17 @@ class Repository extends Component {
         </Owner>
 
         <IssueList>
+          <IssueFilter active={filterIndex}>
+            {filters.map((filter, index) => (
+              <button
+                type="button"
+                key={filter.label}
+                onClick={() => this.handleFilterClick(index)}
+              >
+                {filter.label}
+              </button>
+            ))}
+          </IssueFilter>
           {issues.map(issue => (
             <li key={String(issue.id)}>
               <img src={issue.user.avatar_url} alt={issue.user.login} />
@@ -70,6 +167,23 @@ class Repository extends Component {
             </li>
           ))}
         </IssueList>
+        <PageActions>
+          <button
+            type="button"
+            disabled={disabledNext}
+            onClick={() => this.handlePage('back')}
+          >
+            Anterior
+          </button>
+          <span>Página {page}</span>
+          <button
+            type="button"
+            onClick={() => this.handlePage('next')}
+            disabled={disabledBack}
+          >
+            Próximo
+          </button>
+        </PageActions>
       </Container>
     );
   }
--- src/pages/Repository/styles.js
+++ src/pages/Repository/styles.js
@@ -87,9 +87,6 @@ export const IssueList = styled.ul`
         padding: 2px 4px;
         margin-left: 10px;
       }
-
-      a {
-      }
     }
   }
 `;
@@ -104,6 +101,53 @@ export const User = styled.a`
 
     &:hover {
       color: #7159c1;
+    }
+  }
+`;
+
+export const IssueFilter = styled.div`
+  display: flex;
+  justify-content: center;
+  padding-bottom: 15px;
+
+  button {
+    border-radius: 4px;
+    border: 1px solid #7159c1;
+    outline: 0;
+    padding: 8px;
+    margin: 0 0.25rem;
+    background: white;
+    color: #7159c1;
+    &:nth-child(${props => props.active + 1}) {
+      background: #7159c1;
+      color: white;
+      font-weight: bold;
+    }
+  }
+`;
+
+export const PageActions = styled.footer.attrs(props => ({
+  disabled: props.disabledNext || props.disabledBack,
+}))`
+  padding-top: 15px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 12px;
+  color: #7159c1;
+
+  button {
+    transition: opacity 0.25s ease-out;
+    border-radius: 4px;
+    outline: 0;
+    border: 0;
+    padding: 8px;
+    background: #7159c1;
+    color: white;
+
+    &[disabled] {
+      opacity: 0.35;
+      cursor: not-allowed;
     }
   }
 `;
